de157edb80d5f7c7558d1ef70db9baf287cc6127
Raphael Leandro <releandro15@gmail.com>
2022-11-10 12:47:11 -0400

    Atualizações

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ pd_cotacoes_dollar = pd.json_normalize(cotacoes_dollar)
 print("Nomalizando nomes Dollar")
 pd_normalize_teams = pd.read_excel(r'C:\Users\ran_l\OneDrive\Pessoal\Pense e Enriqueça\Cotações\Nomalize.xlsm', sheet_name='Dollar')
 
-
+"""
+#Metodo antigo de normalização de nomes dos times
 for index_cotacoes, row_cotacoes in pd_cotacoes_dollar.iterrows():
     altera_home = ''
     altera_away = ''
@@ -59,6 +60,28 @@ for index_cotacoes, row_cotacoes in pd_cotacoes_dollar.iterrows():
             (altera_home if altera_home != '' else row_cotacoes['home_team'])[:10] + '_X_' +
             (altera_away if altera_away != '' else row_cotacoes['away_team'])[:10])).replace(' ', '').replace(
                                     '-', '').replace('.', '') + '#' + row_cotacoes['chave_jogo'].split('#')[1]
+"""
+
+#Novo método de normalização de nomes
+m = pd.merge(pd_cotacoes_dollar, pd_normalize_teams, how='left', left_on=['home_team'], right_on=['Dollar'])
+for index_m, row_m in m.iterrows():
+    if pd.isna(row_m['Betano']):
+        m.iloc[index_m, m.columns.get_loc('Betano')] = row_m['home_team']
+m = m.drop(columns=['home_team', 'Dollar', 'Confere']).rename(columns={'Betano': 'home_team'})
+
+m = pd.merge(m, pd_normalize_teams, how='left', left_on=['away_team'], right_on=['Dollar'])
+for index_m, row_m in m.iterrows():
+    if pd.isna(row_m['Betano']):
+        m.iloc[index_m, m.columns.get_loc('Betano')] = row_m['away_team']
+m = m.drop(columns=['away_team', 'Dollar', 'Confere']).rename(columns={'Betano': 'away_team'})
+
+for index_m, row_m in m.iterrows():
+    m.iloc[index_m, m.columns.get_loc('chave_jogo')] = (unidecode.unidecode(
+        row_m['home_team'][:10] + '_X_' +
+        row_m['away_team'][:10])).replace(' ', '').replace(
+        '-', '').replace('.', '') + '#' + row_m['chave_jogo'].split('#')[1]
+
+pd_cotacoes_dollar = m[['id', 'home_team', 'away_team', 'chave_jogo', 'start_date', 'tipo_dollar', 'odds_dollar']]
 
 m = pd.merge(pd_cotacoes_betano, pd_cotacoes_dollar, how='inner', on='chave_jogo')
 
